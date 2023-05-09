2113ffcf743e4f77d15a52eb23555758d153400f
Anna Svensson <anna.el.svensson@outlook.com>
2023-05-09 09:58:10 +0200

    implemented chakraUI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
+import { Button, Heading, VStack } from '@chakra-ui/react'
+
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack>
+      <Heading>Javascript App</Heading>
+      <Button>Test</Button>
+    </VStack>
   );
 }
 
