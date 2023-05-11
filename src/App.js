ddf55743537cf11fab7eaa4c8852def11982f0db
Anna Svensson <anna.el.svensson@outlook.com>
2023-05-11 08:50:24 +0200

    back to start to change the apps purpose

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Heading, VStack } from '@chakra-ui/react'
-import FactorialFunction from './FactorialFunction';
 
 export default function App() {
   return (
     <VStack>
       <Heading as={"h1"} size={"2xl"}>Javascript App</Heading>
-      <FactorialFunction />
     </VStack>
   );
 }
